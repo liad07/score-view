1840024abd7dfde345d8e540607a9e971a515778
liadk07 <91607695+liad07@users.noreply.github.com>
2022-03-29 14:11:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function back() {
     location.replace("https://liad07.github.io/king-of-the-hamuzim/")
 }
 function sharewithme(){
-    var link="mailto:liadgames@gmail.com?subject=i%20want%20to%20share%20with%20you%20my%20record&body=my record is: "+x;
+    var link="mailto:liadgames07@gmail.com?subject=i%20want%20to%20share%20with%20you%20my%20record&body=my record is: "+x;
     location.replace(link)
 
 }
